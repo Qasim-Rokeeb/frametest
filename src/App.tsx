ca15e583293f4a02f9c1d0b13df2825c861999e4
Qasim Rokeeb <qasimrokeeb345@gmail.com>
2025-05-09 07:35:24 +0100

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { sdk } from "@farcaster/frame-sdk";
 import { useEffect } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
   useEffect(() => {
